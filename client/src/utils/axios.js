e87cc657294d468738d98962181e016cc4dd5dbd
Sachin Sharma <sharmasachin705063@gmail.com>
2026-09-06 23:02:12 +0530

    Fix API URL for local and production

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:5000/api"
+    : "https://bookmyevent-backend-u9li.onrender.com/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL,
   headers: {
     "Content-Type": "application/json",
   },
@@ -9,9 +14,11 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
